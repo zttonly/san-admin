031bfa43131156c5fb58a46f2d96bba6a4edf4e3
zhangtingting12 <zhangtingting12@baidu.com>
2021-12-30 15:16:06 +0800

    修改proxy位置&增加env变量

--- config/proxy.js
+++ config/proxy.js
@@ -2,7 +2,7 @@ const DEV_TARGET_URL = 'https://baidu.com';
 const TEST_TARGET_URL = 'https://baidu.com';
 
 // devServer的proxy代理配置 https://webpack.docschina.org/configuration/dev-server/#devserverproxy
-module.exports = {
+const PROXY = {
     dev: {
         '/api/': {
             target: DEV_TARGET_URL,
@@ -15,5 +15,9 @@ module.exports = {
             changeOrigin: true,
         }
     }
+};
+
+module.exports = {
+    proxy: PROXY[process.env.SANP_APP_ENV || 'dev']
 };
 
